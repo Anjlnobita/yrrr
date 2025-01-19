ad366f555cafc9de1c8ed37c0b5292a98f0638a5
Anjlnobita <jiteshkumar7200@gmail.com>
2025-01-19 07:38:18 +0530

    Update call.py

--- BADMUSIC/core/call.py
+++ BADMUSIC/core/call.py
@@ -101,7 +101,7 @@ async def _clear_(chat_id):
     try:
         await app.send_message(
             chat_id,
-            f"**🎧 ɴᴇᴡ sᴏɴɢ ʟᴀɢᴀᴏ ɴᴀ ʙᴀʙʏ🥀⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣⁣**",
+            f"**ꜱᴏɴɢ ʜᴀꜱ ᴇɴᴅᴇᴅ ɪɴ ᴠᴄ🖤**",
         )
     except Exception as e:
         print(f"Error sending message: {e}")
